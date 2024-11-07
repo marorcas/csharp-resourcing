c2c74c9e22814ff79374232cfd315eaf571300b6
marorcas <ortzmc@gmail.com>
2024-11-07 11:17:18 +1100

    Make first and last name required for temps

--- ResourcingUi/src/components/TempForm/schema.ts
+++ ResourcingUi/src/components/TempForm/schema.ts
@@ -1,8 +1,8 @@
 import * as z from 'zod';
 
 export const schema = z.object({
-    firstName: z.string(),
-    lastName: z.string()
+    firstName: z.string().min(2, { message: "First name is required" }),
+    lastName: z.string().min(2, { message: "Last name is required" })
 });
 
 export type TempFormData = z.infer<typeof schema>;
